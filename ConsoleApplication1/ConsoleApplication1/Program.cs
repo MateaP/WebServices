54d418d378ccbaed5e30067ce10c2135e080339b
unknown <matea@goranb2.edusoft2.com>
2015-07-03 10:42:43 +0200

    xquery c#

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xml.XQuery;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XQueryNavigatorCollection col = new XQueryNavigatorCollection();
+            col.AddNavigator("C:\\Users\\matea\\workspace\\test\\my-file.xml", "doc");
+
+            string query = "for $x in document(\"doc\")/dsKurs/KursZbir return ($x/Oznaka/text(), \" \", $x/Nomin/text(), \" \", $x/Sreden/text(), \" \")";
+
+
+            XQueryExpression xepr = new XQueryExpression(query);
+            string result = xepr.Execute(col).ToXml();
+            string[] res = result.Split(' ');
+            int k = 0;
+            for (int i = 0; i < 32; i++)
+            {
+                Console.WriteLine(res[k++] + " " + res[k++] + " " + res[k++]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
